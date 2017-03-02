93a7b3dbf001bf604bb058828ffde41aa6f97665
John Tasto <john@tasto.net>
2017-03-01 22:24:37 -0800

    Add more sign up validation

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -2,36 +2,39 @@ const jwt = require('jwt-simple')
 const User = require('../models/user')
 const config = require('../config')
 
+const emailValidator = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+const passwordValidator = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
+
+
 function tokenForUser(user) {
   const timestamp = new Date().getTime()
+
   // sub = subject, iat = issued at time
   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret)
 }
 
+
+// The router runs the Passport email/password authenticator as a gatekeeper to
+// this function. If the request makes it here, it is ok to send back a token.
 exports.signin = function(req, res, next) {
-  // User has already had their email and password auth'd
-  // We just need to give them a token
   res.send({ token: tokenForUser(req.user) })
 }
+
 
 exports.signup = function(req, res, next) {
   const email = req.body.email
   const password = req.body.password
 
-  if (!email || !password) {
-    return res.status(422).send({ error: 'You must provide email and password'})
-  }
+  if (!email)                      return res.status(422).send({ error: 'No email provided'})
+  if (!emailValidator.test(email)) return res.status(422).send({ error: 'Malformed email'})
 
-  // See if a user with the given email exists
   User.findOne({ email: email }, function(err, existingUser) {
     if (err) { return next(err) }
 
-    // If a user with email does exist, return an error
-    if (existingUser) {
-      return res.status(422).send({ error: 'Email is in use' })
-    }
+    if (existingUser)                      return res.status(422).send({ error: 'Email is in use' })
+    if (!password)                         return res.status(422).send({ error: 'No password provided'})
+    if (!passwordValidator.test(password)) return res.status(422).send({ error: 'Insecure password'})
 
-    // If a user with email does NOT exist, create and save user record
     const user = new User({
       email: email,
       password: password
@@ -39,8 +42,6 @@ exports.signup = function(req, res, next) {
 
     user.save(function(err) {
       if (err) { return next(err) }
-
-      // Repond to request indicating the user was created
       res.json({ token: tokenForUser(user) })
     })
   })
