3887b55219370bbdfbf24ced5ecfc8dc6072730c
John Tasto <john@tasto.net>
2017-03-23 20:35:05 -0700

    Add test for /signup

--- src/controllers/__tests__/authentication.test.js
+++ src/controllers/__tests__/authentication.test.js
@@ -1,21 +1,34 @@
 /* eslint-env jest */
 
+const request = require('supertest')
 const mongoose = require('mongoose')
 const app = require('../../app')
 
 const User = mongoose.model('user')
 
+
 describe('Controller: authentication', () => {
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     await User.remove({})
   })
 
-  test('test', () => {
-    console.log('here')
+  describe('SignUp', () => {
+
+    test('Post to /signup creates a new user', async () => {
+      const preCount = await User.count()
+      const response = await request(app)
+        .post('/signup')
+        .send({
+          email: 'test@test.com',
+          password: 'Password1',
+        })
+      const postCount = await User.count()
+
+      expect(response.status).toBe(200)
+      expect(postCount).toBe(preCount + 1)
+    })
+
   })
 
-  test('test2', () => {
-    console.log('here2')
-  })
 })
