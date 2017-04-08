d2ac297ba3d03bd878a66e97879f273168e590e8
John Tasto <john@tasto.net>
2017-04-07 22:32:38 -0700

    Explicitly close mongo connection after tests

--- tools/setupTests.js
+++ tools/setupTests.js
@@ -6,8 +6,14 @@ mongoose.Promise = Promise
 beforeAll(done => {
   mongoose.connect(process.env.MONGO_TEST_URI || 'mongodb://localhost/auth_test')
   mongoose.connection
-    .once('open', () => done())
+    .once('open', done)
     .on('error', error => {
       console.warn('Warning', error)
     })
+})
+
+afterAll(done => {
+  mongoose.disconnect()
+  mongoose.connection
+    .once('disconnected', done)
 })
