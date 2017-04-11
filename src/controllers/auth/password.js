789c9da530105d3af7e3e09a834cc355b4287347
John Tasto <john@tasto.net>
2017-04-11 02:49:40 -0700

    Fix user not being signed out on password reset

--- src/controllers/auth/password.js
+++ src/controllers/auth/password.js
@@ -56,7 +56,7 @@ module.exports.reset = (req, res, next) => {
         {
           $set: {
             password: hashedPassword,
-            resetTokens: [],
+            refreshTokens: [],
           },
           $unset: {
             resetPasswordToken: '',
