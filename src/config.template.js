56616815b08cda3ccf3c22bdb9f8209633b34822
John Tasto <john@tasto.net>
2017-04-12 19:32:26 -0700

    Fix email links

--- src/config.template.js
+++ src/config.template.js
@@ -1,19 +1,21 @@
 /* eslint-disable key-spacing */
 
 module.exports = {
-  rootURL: process.env.ROOT_URL || 'http://localhost:9090',
-  hostname: process.env.HOSTNAME || 'localhost',
   port: process.env.PORT || 9090,
   mongoURI: process.env.MONGO_URI || 'mongodb://localhost/auth',
   jwt: {
     secret: process.env.JWT_SECRET || '123456', // put something long and random here
     refreshExpiry:       [7,  'days'],
     accessExpiry:        [15, 'minutes'],
-    verifyEmailExpiry:   [1,  'days'],
-    resetPasswordExpiry: [1,  'hours'],
+    verifyEmailExpiry:   [24, 'hours'],
+    resetPasswordExpiry: [30, 'minutes'],
   },
-  mailgun: {
-    apiKey: process.env.MAILGUN_API_KEY || 'key-123456',
-    domain: process.env.MAILGUN_DOMAIN || 'sandbox123456.mailgun.org',
+  mail: {
+    from: process.env.MAIL_FROM || 'Auth JWT <machine@localhost>',
+    linkRootURL: process.env.LINK_ROOT_URL || 'http://localhost:8080',
+    mailgun: {
+      apiKey: process.env.MAILGUN_API_KEY || 'key-123456',
+      domain: process.env.MAILGUN_DOMAIN || 'sandbox123456.mailgun.org',
+    },
   },
 }
--- src/services/mail.js
+++ src/services/mail.js
@@ -1,5 +1,5 @@
 const config = require('../config')
-const mailgun = require('mailgun-js')(config.mailgun)
+const mailgun = require('mailgun-js')(config.mail.mailgun)
 
 
 const sendMail = message =>
@@ -11,12 +11,15 @@ const sendMail = message =>
   })
 
 module.exports.sendEmailVerificationLink = (email, token) => {
+  const expiry = `${config.jwt.verifyEmailExpiry[0]} ${config.jwt.verifyEmailExpiry[1]}`
   const message = {
-    from: `Auth JWT <machine@${config.hostname}>`,
+    from: config.mail.from,
     to: email,
     subject: 'Verify your email',
-    text: 'Follow this link to verify your email and activate your account:' +
-          `${config.rootURL}/verify/${token}`,
+    text: 'Follow this link to verify your email and activate your account:\n\n' +
+          `${config.mail.linkRootURL}/verifyemail/${token}\n\n` +
+          'If you did not request this you can safely ignore it.\n' +
+          `This link will expire in ${expiry}.`,
   }
   return sendMail(message)
 }
@@ -24,12 +27,12 @@ module.exports.sendEmailVerificationLink = (email, token) => {
 module.exports.sendResetPasswordLink = (email, token) => {
   const expiry = `${config.jwt.resetPasswordExpiry[0]} ${config.jwt.resetPasswordExpiry[1]}`
   const message = {
-    from: `Auth JWT <machine@${config.hostname}>`,
+    from: config.mail.from,
     to: email,
     subject: 'Reset your password',
-    text: 'Follow this link to reset your password:' +
-          `${config.rootURL}/verify/${token}` +
-          'If you did not request this you can safely ignore it.' +
+    text: 'Follow this link to reset your password:\n\n' +
+          `${config.mail.linkRootURL}/resetpassword/${token}\n\n` +
+          'If you did not request this you can safely ignore it.\n' +
           `This link will expire in ${expiry}.`,
   }
   return sendMail(message)
